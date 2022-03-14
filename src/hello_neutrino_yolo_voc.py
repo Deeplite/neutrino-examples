bbd0b7db528efdf0067a2b7c2e077b57b83d8255
ivan-lazarevich <92095665+ivan-lazarevich@users.noreply.github.com>
2022-03-14 10:24:32 +0300

    Add yolo5_6(l,x) options to VOC example (#13)

--- src/hello_neutrino_yolo_voc.py
+++ src/hello_neutrino_yolo_voc.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     parser.add_argument('-b', '--batch_size', type=int, metavar='N', default=8, help='mini-batch size')
     parser.add_argument('-j', '--workers', type=int, metavar='N', default=4, help='number of data loading workers')
     parser.add_argument('-a', '--arch', metavar='ARCH', default='yolo5m', help='model architecture',
-        choices=['yolo4s', 'yolo4m', 'yolo5_6s', 'yolo5_6m'])
+        choices=['yolo4s', 'yolo4m', 'yolo5_6s', 'yolo5_6m', 'yolo5_6l', 'yolo5_6x'])
 
     # neutrino args
     parser.add_argument('-d', '--delta', type=float, metavar='DELTA', default=0.05, help='accuracy drop tolerance')

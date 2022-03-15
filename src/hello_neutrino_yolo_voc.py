0f96a570926caabc1342cb24d507911845fc293f
ivan-lazarevich <92095665+ivan-lazarevich@users.noreply.github.com>
2022-03-15 03:10:51 +0300

    correct defaults for yolo examples (#14)

--- src/hello_neutrino_yolo_voc.py
+++ src/hello_neutrino_yolo_voc.py
@@ -51,8 +51,8 @@ if __name__ == '__main__':
                         help='vockit data path contains VOC2007 and VOC2012.')
     parser.add_argument('-b', '--batch_size', type=int, metavar='N', default=8, help='mini-batch size')
     parser.add_argument('-j', '--workers', type=int, metavar='N', default=4, help='number of data loading workers')
-    parser.add_argument('-a', '--arch', metavar='ARCH', default='yolo5m', help='model architecture',
-        choices=['yolo4s', 'yolo4m', 'yolo5_6s', 'yolo5_6m', 'yolo5_6l', 'yolo5_6x'])
+    parser.add_argument('-a', '--arch', metavar='ARCH', default='yolo3', help='model architecture',
+        choices=['yolo3', 'yolo4s', 'yolo4m', 'yolo5_6s', 'yolo5_6m', 'yolo5_6l', 'yolo5_6x'])
 
     # neutrino args
     parser.add_argument('-d', '--delta', type=float, metavar='DELTA', default=0.05, help='accuracy drop tolerance')

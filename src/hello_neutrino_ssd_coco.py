c6cbc023426608be93f73cdf0b25a7b758cae525
Alexander Hoffman <alexander.hoffman@deeplite.ai>
2022-09-01 14:32:25 -0400

    combine ssd coco and ssd voc scripts

--- src/hello_neutrino_ssd_coco.py
+++ src/hello_neutrino_ssd_coco.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from pathlib import Path
 
 from pycocotools.coco import COCO
 
@@ -14,33 +15,45 @@ from deeplite_torch_zoo import get_data_splits_by_name, get_model_by_name, get_e
 
 from deeplite_torch_zoo.src.objectdetection.ssd.repo.vision.nn.multibox_loss import MultiboxLoss
 from deeplite_torch_zoo.src.objectdetection.ssd.config.mobilenetv1_ssd_config import MOBILENET_CONFIG
-
+from deeplite_torch_zoo.src.objectdetection.ssd.config.vgg_ssd_config import VGG_CONFIG
 
 
 logger = getLogger(__name__)
 
 
 class Eval(TorchEvaluationFunction):
-    def __init__(self, model_name):
-        self._evaluation_fn = get_eval_function(model_name=model_name, dataset_name='coco')
+    def __init__(self, model_name, dataset_name, data_root, num_classes):
+        self._evaluation_fn = get_eval_function(model_name=model_name, dataset_name=dataset_name)
+        self._gt = None
+        self._data_root = data_root
+        if dataset_name == 'coco':
+            self._gt = COCO(Path(self._data_root) / "annotations/instances_val2017.json")
+        self._num_classes = num_classes
 
     def _compute_inference(self, model, data_loader, **kwargs):
         # silent **kwargs
-        cocoGt = COCO(f"{args.coco_path}/{args.annotation_file}")
         data_loader = data_loader.native_dl
-        return self._evaluation_fn(model=model, data_loader=data_loader, gt=cocoGt)
+        eval_kwargs = {'model': model, 'data_loader': data_loader}
+        if self._gt:
+            eval_kwargs['gt'] = self._gt
+        return self._evaluation_fn(**eval_kwargs)
 
 
 class SSDLoss(LossFunction):
-    def __init__(self, device="cuda"):
-        config = MOBILENET_CONFIG()
+    def __init__(self, model_name, device="cuda"):
+        if 'vgg' in model_name:
+            config = VGG_CONFIG()
+        elif 'mb' in model_name:
+            config = MOBILENET_CONFIG()
+        else:
+            raise ValueError('example only supports mobilenet, vgg based ssd models')
         self.torch_device = device
         self.criterion = MultiboxLoss(config.priors, iou_threshold=0.5, neg_pos_ratio=3,
                                       center_variance=0.1, size_variance=0.2, device=device)
 
     def __call__(self, model, data):
         model.to(self.torch_device)
-        images, boxes, labels, _ = data
+        images, boxes, labels = data[0], data[1], data[2]
         images, boxes, labels = images.to(self.torch_device), boxes.to(self.torch_device), labels.to(self.torch_device)
 
         confidence, locations = model(images)
@@ -52,13 +65,15 @@ class SSDLoss(LossFunction):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # model/dataset args
-    parser.add_argument('--coco_path', default='/neutrino/datasets/coco2017/', help='path to the dataset')
-    parser.add_argument('--dataset-type', default='coco', choices=['coco'])
-    parser.add_argument('--annotation-file', default='annotations/instances_val2017.json',
-        choices=['test_data_COCO.json', 'annotations/instances_val2017.json'])
+    parser.add_argument('-r', '--data_root', default='/neutrino/datasets/coco2017/', help='path to the dataset')
+    parser.add_argument('--dataset', default='coco', choices=['coco', 'voc'])
+    # parser.add_argument('--annotation-file', default='annotations/instances_val2017.json',
+        # choices=['test_data_COCO.json', 'annotations/instances_val2017.json'])
     parser.add_argument('-b', '--batch_size', type=int, metavar='N', default=8, help='mini-batch size')
     parser.add_argument('-j', '--workers', type=int, metavar='N', default=4, help='number of data loading workers')
-    parser.add_argument('-a', '--arch', metavar='ARCH', default='mb2_ssd', help='model architecture')
+    parser.add_argument('-a', '--arch', metavar='ARCH', default='mb2_ssd', 
+        choices=('mb2_ssd', 'mb1_ssd', 'mb2_ssd_lite', 'vgg16_ssd'),
+        help='model architecture, coco only supported for mb2_ssd, other choices for VOC')
 
     # neutrino args
     parser.add_argument('-d', '--delta', type=float, metavar='DELTA', default=0.05, help='accuracy drop tolerance')
@@ -72,18 +87,23 @@ if __name__ == '__main__':
     device_map = {'CPU': 'cpu', 'GPU': 'cuda'}
 
     data_splits = get_data_splits_by_name(
-        data_root=args.coco_path,
-        dataset_name=args.dataset_type,
+        data_root=args.data_root,
+        dataset_name=args.dataset,
         model_name=args.arch,
         batch_size=args.batch_size,
         num_workers=args.workers,
         device=device_map[args.device],
-
     )
-    fp = TorchForwardPass(model_input_pattern=(0, '_', '_', '_'))
-    num_classes = data_splits['train'].dataset.num_classes - 1
+    
+    if args.dataset == 'voc':
+        fp = TorchForwardPass(model_input_pattern=(0, '_', '_'))
+        num_classes = 20
+    else:
+        fp = TorchForwardPass(model_input_pattern=(0, '_', '_', '_'))
+        num_classes = 80
+
     reference_model = get_model_by_name(model_name=args.arch,
-                                        dataset_name=args.dataset_type,
+                                        dataset_name=args.dataset,
                                         pretrained=True,
                                         progress=True,
                                         device=device_map[args.device],)
@@ -94,11 +114,12 @@ if __name__ == '__main__':
         def eval_func(model, data_splits):
             return {eval_key: 1}
     else:
-        eval_func = Eval(model_name=args.arch)
+        eval_func = Eval(model_name=args.arch, dataset_name=args.dataset,
+                         data_root=args.data_root, num_classes=num_classes)
 
     # loss
     loss_cls = SSDLoss
-    loss_kwargs = {'device': device_map[args.device]}
+    loss_kwargs = {'device': device_map[args.device], 'model_name': args.arch}
 
     # custom config
     config = {'deepsearch': args.deepsearch,
